32d32044f0c16ed303d6ec56b54f171dcd38bacf
RDOToole89 <roibinotoole@gmail.com>
2021-01-05 20:55:17 +0100

    Modified board component to use v-for loop

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,32 +1,8 @@
 <template>
   <div class="game">
     <div class="game__board">
-      <div class="game__box">
-        <span>{{ squares[0] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[1] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[2] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[3] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[4] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[5] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[6] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[7] }}</span>
-      </div>
-      <div class="game__box">
-        <span>{{ squares[8] }}</span>
+      <div class="game__box" v-for="(squareValue, position) in squares" :key="position">
+        <span>{{ squareValue }}</span>
       </div>
     </div>
   </div>
